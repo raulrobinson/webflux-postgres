1ac2d4862f5631169c1eb4cc87a1cb1ed5499b56
Raul Bolivar Navas <raul.bolivar@pragma.com.co>
2025-09-17 11:30:24 -0500

    ic

--- webflux/src/main/resources/data.sql
+++ webflux/src/main/resources/data.sql
@@ -1,1 +1,6 @@
 INSERT INTO users (name, email) VALUES ('Raul', 'raul@test.com');
+INSERT INTO users (name, email) VALUES ('Ana', 'ana@test.com');
+INSERT INTO users (name, email) VALUES ('Luis', 'luis@test.com');
+INSERT INTO users (name, email) VALUES ('Lucia', 'lucia@test.com');
+INSERT INTO users (name, email) VALUES ('Jorge', 'jorge@test.com');
+INSERT INTO users (name, email) VALUES ('Marta', 'marta@test.com');
